a0943bf4b93bf41e3bdec23f8658a5c1eae1f818
Carlos Martinez-Cabello <79131673+carlosmmc@users.noreply.github.com>
2024-01-19 15:08:42 -0800

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,76 @@
+from dotenv import load_dotenv
+import os
+import datetime
+
+from flask import Flask, render_template, request
+from google.auth.transport import requests
+from google.cloud import datastore
+import google.oauth2.id_token
+
+app = Flask(__name__)
+datastore_client = datastore.Client()
+firebase_request_adapter = requests.Request()
+load_dotenv()
+
+firebase_config = {
+    "apiKey": os.environ.get("FIREBASE_API_KEY"),
+    "authDomain": os.environ.get("FIREBASE_AUTH_DOMAIN"),
+    "projectId": os.environ.get("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.environ.get("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.environ.get("FIREBASE_APP_ID")
+}
+
+@app.route("/")
+def root():
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    times = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter
+            )
+
+            store_time(claims["email"], datetime.datetime.now(tz=datetime.timezone.utc))
+            times = fetch_times(claims["email"], 10)
+
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+    return render_template("index.html", user_data=claims, error_message=error_message, times=times, firebase_config=firebase_config)
+
+def store_time(email, dt):
+    entity = datastore.Entity(key=datastore_client.key("User", email, "visit"))
+    entity.update({"timestamp": dt})
+
+    datastore_client.put(entity)
+
+def fetch_times(email, limit):
+    ancestor = datastore_client.key("User", email)
+    query = datastore_client.query(kind="visit", ancestor=ancestor)
+    query.order = ["-timestamp"]
+
+    times = query.fetch(limit=limit)
+
+    return times
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
