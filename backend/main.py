539dbfb17061d17630662406d25710ad95dfaa3b
Carlos Martinez <79131673+carlosmmc@users.noreply.github.com>
2024-02-04 15:05:27 -0800

    work

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,34 @@
 """entry point for flask backend"""
 
 import datetime
+import google.auth
 
 from dotenv import load_dotenv
 from flask import Flask, render_template, g
 
-from db.cloud_sql import get_user_passwords
-from db.firebase import get_firebase_config, store_time, fetch_times
-from middleware.auth import init_auth_middleware
 from api.accounts import accounts_blueprint
 from api.credentials import credentials_blueprint
+from db.cloud_sql import get_user_passwords
+from db.firebase import get_firebase_config, store_time, fetch_times
+import google.cloud.logging as cloud_logging
+from middleware.auth import init_auth_middleware
+from middleware.api_observability import emit_api_observability
+
+_, project = google.auth.default()
 
 load_dotenv()
 app = Flask(__name__)
 
 # register middleware
 init_auth_middleware(app)
+app.after_request(emit_api_observability)
 
 # register API methods
 app.register_blueprint(accounts_blueprint)
 app.register_blueprint(credentials_blueprint)
+
+client = cloud_logging.Client(project=project)
+logger = client.logger("MAIN_APPLICATION")
 
 
 @app.route("/")
@@ -44,6 +53,8 @@ def root():
         password_info=password_info,
     )
 
+
+logger.log_text("!! flask backend started successfully !!", severity="INFO")
 
 if __name__ == "__main__":
     # for local development only
