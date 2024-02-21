2b050798a7dcf03cc749c4312649f49d4ab3d9af
Carlos Martinez <79131673+carlosmmc@users.noreply.github.com>
2024-02-20 20:13:38 -0800

    changes

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,12 @@
 """entry point for flask backend"""
 
-import datetime
 import google.auth
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, g
+from flask import Flask
 
 from api.accounts import accounts_blueprint
 from api.credentials import credentials_blueprint
-from db.cloud_sql import get_user_passwords
-from db.firebase import get_firebase_config, store_time, fetch_times
 import google.cloud.logging as cloud_logging
 from middleware.auth import init_auth_middleware
 from middleware.api_observability import emit_api_observability
@@ -17,10 +14,10 @@ from middleware.api_observability import emit_api_observability
 _, project = google.auth.default()
 
 load_dotenv()
-app = Flask(__name__)
+app = Flask(__name__, static_folder="build", static_url_path="/")
 
 # register middleware
-init_auth_middleware(app)
+# init_auth_middleware(app)
 app.after_request(emit_api_observability)
 
 # register API methods
@@ -33,25 +30,9 @@ logger = client.logger("MAIN_APPLICATION")
 
 @app.route("/")
 def root():
-    user = g.user if g.user else None
-    current_date_time = None
-    times = None
-    password_info = None
-
-    if user:
-        user_email_address = user["email"]
-        current_date_time = datetime.datetime.now(tz=datetime.timezone.utc)
-        store_time(user_email_address, current_date_time)
-        times = fetch_times(user_email_address, 5)
-        password_info = get_user_passwords(user_email_address, limit=5)
-
-    return render_template(
-        "index.html",
-        user_data=user,
-        times=times,
-        firebase_config=get_firebase_config(),
-        password_info=password_info,
-    )
+    # to get user once we add auth back in
+    # user = g.user if g.user else None
+    return app.send_static_file("index.html")
 
 
 logger.log_text("!! flask backend started successfully !!", severity="INFO")
