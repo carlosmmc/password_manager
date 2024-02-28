c2d1125f1c3baf8a3b4b863fbfc2a0d57f015652
Zhimei Wang <happywzm@gmail.com>
2024-02-27 21:11:03 -0500

    removed hard coded info

--- frontend/src/components/AccountList.jsx
+++ frontend/src/components/AccountList.jsx
@@ -5,12 +5,14 @@ import { Col } from "react-bootstrap";
 const AccountList = () => {
   // do decryption in here
 
+  const baseLink = "password-manager-osu.wl.r.appspot.com/api/v1/accounts/";
+  const accountId = "ACCOUNTID";
   const [apps, setApps] = useState([]);
   const loadApps = async () => {
-    const response = await fetch("password-manager-osu.wl.r.appspot.com/api/v1/accounts/a8fc5384-1dbe-4763-b0ab-40bffb02f5a4/items")
+    const response = await fetch(baseLink + accountId + "/items");
+    const data = await response.json()
 
-    // const data = await response.body.json();
-    setApps([{"cty":"b5+jwk+json","data":"yzab","enc":null,"id":"f37e85bf-cd37-4fe7-a684-93659a0e7d2c","kid":"27342754-60e8-4f9b-8be9-61c8d7dc3041","self":"http://password-manager-osu.wl.r.appspot.com/api/v1/accounts/a8fc5384-1dbe-4763-b0ab-40bffb02f5a4/items/f37e85bf-cd37-4fe7-a684-93659a0e7d2c"}]);
+    setApps(data);
   };
   useEffect(() => {
     loadApps();
